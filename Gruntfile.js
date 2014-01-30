b459148e817b92c7a1dda38e8f0fab2d635c08cf
kevinrabinovich <githubcommits@kevinrabinovich.me>
2014-01-30 16:09:06 -0500

    Updated Grunt.JS settings, adding grunt-notify
    Added grunt-notify to Gruntfile.js, Grunt.JS configuration file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,5 +38,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-notify');
 	grunt.registerTask('default', ['sass', 'autoprefixer']);
 };
